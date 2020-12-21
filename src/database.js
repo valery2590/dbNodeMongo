c1f78f8314b831cdc9c2d84e60e6c9dbf3abb5b0
Valery FH <valeryfigueroah@gmail.com>
2020-12-21 21:06:02 +0100

    changes

---
+++ src/database.js
@@ -0,0 +1,14 @@
+const mongoose = require ('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+const URI = 'mongodb://localhost/proyecto_propioBackend';
+
+mongoose.connect(URI)
+    .then(db => console.log('DB is connected'))
+    .catch( err => console.log(err));
+
+
+module.exports = mongoose;
