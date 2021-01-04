c24a4bf33f94668fb6b874da6240c566251b1099
Valery FH <valeryfigueroah@gmail.com>
2021-01-04 21:36:07 +0100

    last changes

--- src/app/app.js
+++ src/app/app.js
@@ -1,174 +1,196 @@
-import { Mongoose } from 'mongoose';
-import React , { Component} from 'react';
-
+import { Mongoose } from "mongoose";
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      description: "",
+      tasks: [],
+      _id: "",
+    };
+    this.addTask = this.addTask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    constructor (){
-        super();
-        this.state ={
-            title: '',
-            description: '',
-            tasks: [],
-            _id: ''
-        };
-        this.addTask = this.addTask.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    addTask(e){
-        if (this.state._id){
-            fetch(`/api/tasks/${this.state._id}`,{
-                method: 'PUT',
-                body: JSON.stringify(this.state),
-                headers : {
-                    'Accept' : 'application/json',
-                    'Content-Type':'application/json'
-            }
+  addTask(e) {
+    if (this.state._id) {
+      fetch(`/api/tasks/${this.state._id}`, {
+        method: "PUT",
+        body: JSON.stringify(this.state),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          M.toast({ html: "Task Updated" });
+          console.log(data);
+          this.setState({ title: "", description: "", _id: "" });
+          this.fetchTasks();
+        });
+    } else {
+      fetch("/api/tasks", {
+        method: "POST",
+        body: JSON.stringify(this.state),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          M.toast({ html: "Task Saved" });
+          this.setState({ title: "", description: "" });
+          this.fetchTasks();
         })
-        .then(res => res.json())
-        .then(data => {
-            M.toast({html:'Task Updated'});
-            console.log(data);
-            this.setState({title:'', description:'',_id:''} )
-            this.fetchTasks();
-        });
-        }else{
-            fetch('/api/tasks',{
-                method:'POST',
-                body: JSON.stringify(this.state),
-                headers : {
-                    'Accept' : 'application/json',
-                    'Content-Type':'application/json'
-                }
-            })
-                .then (res => res.json())
-                .then (data => {
-                    console.log(data)
-                    M.toast({html : 'Task Saved'})
-                    this.setState({title:'', description:''});
-                    this.fetchTasks();
-                })
-                
-                .catch (err => console.log(err))
-        }
-        e.preventDefault();
-    }
- 
 
-    componentDidMount(){
-       this.fetchTasks(); 
+        .catch((err) => console.log(err));
     }
+    e.preventDefault();
+  }
 
-    fetchTasks(){
-        fetch('/api/tasks')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            this.setState({tasks:data});
-            console.log(this.state.tasks)
-    });
-}
-    deleteTask(id){
-        if (confirm ('are you sure of it?')){
-            fetch (`/api/tasks/${id}`, {
-                method:'DELETE',
-                headers : {
-                    'Accept' : 'application/json',
-                    'Content-Type':'application/json'
-            }
-        })
-        .then(res => res.json())
-        .then(data => {
-            M.toast({html:'Task Deleted'});
-            console.log(data);
-            this.fetchTasks();
-        });
-        }
-}
-    editTask(id){
-        fetch(`/api/tasks/${id}`)
-        .then (res => res.json())
-        .then(data => {
-            console.log(data)
-            this.setState({
-                title: data.title,
-                description: data.description,
-                _id: data._id
-            })
+  componentDidMount() {
+    this.fetchTasks();
+  }
+
+  fetchTasks() {
+    fetch("/api/tasks")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ tasks: data });
+        console.log(this.state.tasks);
+      });
+  }
+  deleteTask(id) {
+    if (confirm("are you sure of it?")) {
+      fetch(`/api/tasks/${id}`, {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          M.toast({ html: "Task Deleted" });
+          console.log(data);
+          this.fetchTasks();
         });
     }
-
-    handleChange(e){
-        const {name, value} = e.target;
+  }
+  editTask(id) {
+    fetch(`/api/tasks/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
         this.setState({
-            [name]: value
-        })
-    }
-    render(){
-        return (
-            <div>
-                {/*NAVIGATION */}
-                <nav className="light-blue darken-4">
-                    <div className="container">
-                        <a className="brand-logo" href="/">Primera App</a>
-                    </div>
-                </nav> 
+          title: data.title,
+          description: data.description,
+          _id: data._id,
+        });
+      });
+  }
 
-                <div className="container">
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+  render() {
+    return (
+      <div>
+        {/*NAVIGATION */}
+        <nav className="light-blue darken-4">
+          <div className="container">
+            <a className="brand-logo" href="/">
+              Primera App
+            </a>
+          </div>
+        </nav>
+
+        <div className="container">
+          <div className="row">
+            <div className="col s5">
+              <div className="card">
+                <div className="card-content">
+                  <form onSubmit={this.addTask}>
                     <div className="row">
-                        <div className="col s5">
-                            <div className="card">
-                                <div className="card-content">
-                                    <form onSubmit={this.addTask}>
-                                        <div className="row">
-                                            <div className="input-field col s12">
-                                                <input name="title" onChange={this.handleChange} type="text" placeholder="Task Title" value={this.state.title} />
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="input-field col s12">
-                                                <textarea name="description" onChange={this.handleChange} placeholder="Task Description" className="materialize-textarea" value={this.state.description}>
-                                                </textarea>
-                                            </div>
-                                        </div>
-                                        <button className="btn light-blue darken-4" type="submit">Send
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col s7">
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Title</th>
-                                        <th>Description</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        this.state.tasks.map(task => {
-                                            return (
-                                                <tr key={task._id}>
-                                                    <td>{task.title}</td>
-                                                    <td>{task.description}</td>
-                                                    <td>
-                                                        <button className="btn light-blue darken-4"><i className="material-icons" onClick={ () =>this.deleteTask (task._id)} >delete</i></button>
-                                                        <button className="btn light-blue darken-4" style={{margin:'4px'}} onClick={()=> this.editTask(task._id)}><i className="material-icons">edit</i></button>
-                                                    </td>
-                                                </tr>
-                                            )
-                                        })
-                                    }
-                                </tbody>
-                            </table>
-                        </div>
+                      <div className="input-field col s12">
+                        <input
+                          name="title"
+                          onChange={this.handleChange}
+                          type="text"
+                          placeholder="Task Title"
+                          value={this.state.title}
+                        />
+                      </div>
                     </div>
-                </div>  
+                    <div className="row">
+                      <div className="input-field col s12">
+                        <textarea
+                          name="description"
+                          onChange={this.handleChange}
+                          placeholder="Task Description"
+                          className="materialize-textarea"
+                          value={this.state.description}
+                        ></textarea>
+                      </div>
+                    </div>
+                    <button className="btn light-blue darken-4" type="submit">
+                      Send
+                    </button>
+                  </form>
+                </div>
+              </div>
             </div>
-        )
-    }
+            <div className="col s7">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.tasks.map((task) => {
+                    return (
+                      <tr key={task._id}>
+                        <td>{task.title}</td>
+                        <td>{task.description}</td>
+                        <td>
+                          <button className="btn light-blue darken-4">
+                            <i
+                              className="material-icons"
+                              onClick={() => this.deleteTask(task._id)}
+                            >
+                              delete
+                            </i>
+                          </button>
+                          <button
+                            className="btn light-blue darken-4"
+                            style={{ margin: "4px" }}
+                            onClick={() => this.editTask(task._id)}
+                          >
+                            <i className="material-icons">edit</i>
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
