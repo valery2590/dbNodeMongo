1bee1db76fe7c9ba09c5f95606ee53dd3dffd9ba
Valery FH <valeryfigueroah@gmail.com>
2021-01-04 14:47:31 +0100

    backend is complete

--- src/database.js
+++ src/database.js
@@ -4,15 +4,15 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-const URI = 'mongodb://localhost/proyectoPropio_backend';
+const URI = 'mongodb://localhost/proyecto-mern';
 
 mongoose.connect(URI,{
     useNewUrlParser:true,
     useFindAndModify: false,
-   useCreateIndex: true,
-   useUnifiedTopology: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 })
-    .then(db => console.log('DB is connected'))
+    .then(db => console.log('DB is on dude!'))
     .catch( err => console.log(err));
 
 
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,37 @@
 const express = require ('express')
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send({
-        status: 'API Works',
-    });
- });
+const Task = require ('../models/task');
+
+router.get ('/', async (req, res)=>{
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+router.get ('/:id', async (req,res) => {
+   const task =  await Task.findById(req.params.id)
+   res.json (task);
+});
+
+router.post ('/', async (req, res) => {
+    const {title, description} = req.body;
+    const task = new Task ({ title, description});
+    console.log (task);
+    await task.save();
+    res.json( {status: 'Task saved'})
+});
+
+router.put ('/:id', async (req, res) => {
+    const { title, description } = req.body;
+    const newTask = {title, description};
+   await Task.findByIdAndUpdate(req.params.id, newTask);
+    console.log(req.params.id);
+    res.json({status : 'task updated'});
+})
+
+router.delete ( '/:id', async (req,res) => {
+    await Task.findByIdAndRemove (req.params.id);
+    res.json ({status: 'task deleted'});
+})
 
 module.exports = router;
